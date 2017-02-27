a0c8b6e4f74beede27697f3976a7a76b5aad3e1b
Anna812 <88menyus@gmail.com>
2017-02-27 12:44:01 +0100

    create first test: testPrintRouteWithOneDestination

--- test/DestinationTest.java
+++ test/DestinationTest.java
@@ -1,5 +1,15 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by Anna on 17/02/27.
  */
 public class DestinationTest {
+
+    @Test
+    public void testPrintRouteWithOneDestination() throws Exception {
+        Destination x = new Destination("x");
+        assertEquals("x", x.getName());
+    }
 }
