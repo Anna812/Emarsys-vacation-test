fa1f1c3247725d2da9b60d27ea517962ae653338
Anna812 <88menyus@gmail.com>
2017-02-27 12:38:46 +0100

    setup fields, cunstructors and setters to Destination, even a pelimiary route() method

--- src/Destination.java
+++ src/Destination.java
@@ -2,4 +2,31 @@
  * Created by Anna on 17/02/27.
  */
 public class Destination {
+    private String name;
+    private Destination previousDestination;
+
+    public Destination(String name) {
+        this.name = name;
+    }
+
+    public Destination(String name, Destination previousDestination) {
+        this.name = name;
+        this.previousDestination = previousDestination;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Destination getPreviousDestination() {
+        return previousDestination;
+    }
+
+    public String route() {
+        if(previousDestination == null) {
+            return name;
+        } else {
+            return previousDestination.route();
+        }
+    }
 }
