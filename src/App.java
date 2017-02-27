e5a07c5a36dc8399c07ad91e9f84dd64a87cb356
Anna812 <88menyus@gmail.com>
2017-02-27 13:16:11 +0100

    add createRoute method for App class

--- src/App.java
+++ src/App.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
+
 /**
  * Created by Anna on 17/02/27.
  */
 public class App {
     public static void main(String[] args) {
 
+    }
+
+    public static ArrayList<String> createRoute(ArrayList<Destination> destinations) throws Exception {
+        ArrayList<String> route = new ArrayList<>();
+
+        for (Destination temp : destinations) {
+            if(temp.getPreviousDestination() != null) {
+                route.add(temp.route());
+            } else {
+                route.add(temp.getName());
+            }
+        }
+        return route;
     }
 }
